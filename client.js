93961756c1d30fea427597f2759f997f13e9579d
Aden Ali <abdinasir55@gmail.com>
2024-09-26 23:05:09 -0500

    FINAL MODIFIED

--- client.js
+++ client.js
@@ -58,6 +58,24 @@ console.log(tealCount);
  * 4. Output the original array, odd number array and even number array
  */
 
+console.log('Array Numbers, Odd-Numbers, and Even-Numbers:');
+
+let arrayNumbers = ['3', '7', '2', '8', '11', '4', '2']
+let oddNumbers = []; 
+let evenNumbers = []; 
+for (let i = 0; i < arrayNumbers.length; i++ ){
+     if (arrayNumbers[i] % 2 === 0){
+          evenNumbers.push(arrayNumbers[i]);
+     }
+     else {
+          oddNumbers.push(arrayNumbers[i]);
+     }
+}
+
+console.log('Odd Numbers:', oddNumbers);
+console.log('Even Numbers:', evenNumbers);
+console.log('Array Numbers:', arrayNumbers);
+
 
 // Example output
 // 3, 7, 2, 8, 11, 4, 2
@@ -73,6 +91,15 @@ console.log(tealCount);
  * 3. Write a loop that adds the opposite value to the toggled array
  * 4. Output both arrays
  */
+
+let booleanArray = [true, false, true, true];
+let toggled = [];
+for (let Oppositevalue of booleanArray){
+         toggled.push(!Oppositevalue);
+
+     }
+console.log('Boolean Array:', booleanArray);
+console.log('Toggled Array:', toggled);
 
 
 // Example output
@@ -92,6 +119,16 @@ console.log(tealCount);
  * Hint: Try using a while loop for this one.
  */
 
+let stretch = [3, 0, 2, 8, 0, 0, 0]; 
+console.log('STRETCH Remove 0s:', stretch);
+
+while (stretch.length > 0 && stretch[stretch.length -1] === 0){
+
+     stretch.pop(0);
+}
+         
+console.log('Removed 0s from the end:', stretch);
+
 
 // Example output
 // Before loop 3, 0, 2, 8, 0, 0, 0
@@ -108,7 +145,23 @@ console.log(tealCount);
  * 3. Log the highest and lowest number to the console
  */
 
-
+      let largishArray = [2, 2, -3, 7, 4, 1, 7, 12, 8];
+      let highNumber = [0];
+      let lowNumber = [0];
+      
+      for (let array of largishArray ){
+           if (array > highNumber){
+                highNumber = array;
+           }
+           if (array < lowNumber) {
+               
+                lowNumber = array;
+           }
+      }
+      console.log('largish array:', largishArray);
+      console.log('Highes numers:', highNumber);
+      console.log('Lowest Number', lowNumber);
+      
 // Example output
 // 2, 2, -3, 7, 4, 1, 7, 12, 8
 // High: 12
